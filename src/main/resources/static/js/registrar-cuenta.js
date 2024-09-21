e42045fe2345d46814febc7328a6c9ccc62c7472
=HAriass <hernanarias.utn@gmail.com>
2024-09-21 14:50:06 -0300

    html de cuenta creado.

---
+++ src/main/resources/static/js/registrar-cuenta.js
@@ -0,0 +1,45 @@
+const form = document.getElementById('addCuenta');
+
+        function guardarCuenta() {
+            form.addEventListener('submit', function(event) {
+                event.preventDefault(); // Previene el comportamiento por defecto del formulario
+
+                // Captura los datos del formulario
+                const formData = new FormData(form);
+
+                // Convierte los datos del formulario a un objeto
+                const data = {};
+                formData.forEach((value, key) => {
+                    data[key] = value;
+                });
+                
+                //para que funcione, el tipo de cuenta tiene que estar tal cual.. 
+                data['tipoCuentaModel'] =
+                {
+                    id: 2,
+                    nombre: "cliente",
+                    descripcion: "no tiene acceso a administracion del sistema"
+                };
+
+                // Convierte el objeto a JSON
+                const jsonData = JSON.stringify(data);
+
+                // Envía los datos JSON usando Axios
+                axios.post("/cuenta/guardarCuenta", jsonData, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                })
+                .then(response => {
+                    console.log('Éxito:', response.data);
+                    //window.location.href='/registrarEstado';
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+                
+            });
+        }
+
+        // Llama a la función para añadir el listener
+        guardarCuenta();
