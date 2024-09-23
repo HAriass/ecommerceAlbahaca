7d038cfd5fdd296168bd4c82c59f61cdcfad91e8
Facundo Sanchez <105307321+facusanchez17@users.noreply.github.com>
2024-09-23 16:37:38 -0300

    botones

--- src/main/resources/static/js/categorias.js
+++ src/main/resources/static/js/categorias.js
@@ -1,44 +1,44 @@
-        function getCategorias() {
-            axios.get("/categoria/listarCategorias")
-            .then(function(response) {
-                const categorias = response.data;
-                const tbody = document.querySelector("tbody");
-                let htmlContent = '';
-                categorias.forEach(categoria => {
-                    htmlContent += `
+function getCategorias() {
+    axios.get("/categoria/listarCategorias")
+        .then(function (response) {
+            const categorias = response.data;
+            const tbody = document.querySelector("tbody");
+            let htmlContent = '';
+            categorias.forEach(categoria => {
+                htmlContent += `
                         <tr>
                             <td>${categoria.nombre}</td>
                             <td>${categoria.descripcion}</td>
-                            <td><a href="https://drive.google.com/uc?export=view&id=${categoria.imagen}" target="_blank">Ver Imagen</a></td>
+                            <td><button class="btn-ver" onclick="window.open('https://drive.google.com/uc?export=view&id=${categoria.imagen}', '_blank')">Ver Imagen</button></td>
                             <td><button class="btn-modificar" onclick="location.href='/modificarCategoria/${categoria.id}'">Modificar</button></td>
                             <td><button class="btn-eliminar" onclick="eliminarCategoria(${categoria.id})">Eliminar</button></td>
                         </tr>
                     `;
-                });
-                tbody.innerHTML = htmlContent;
-            })
-            .catch((err) => console.error(err));
-        }
-        
-        function eliminarCategoria(id) {
-            axios.delete(`/categoria/eliminarCategoria/${id}`)
-            .then(response => {
-                console.log('Éxito:', response.data);
-                alert('Categoría Eliminada exitosamente!');
-                window.location.href='/registrarCategoria';
-            })
-            .catch(error => {
-                console.error('Error:', error);
             });
-        }
-        function getCategoriaById(id) {
-            axios.get(`/categoria/obtenerCategoriaPorId/${id}`)
-            .then(function(response) {
-                const categoria = response.data;
-                const tbody = document.querySelector("tbody");
-                let htmlContent = '';
-                if (categoria) {
-                    htmlContent += `
+            tbody.innerHTML = htmlContent;
+        })
+        .catch((err) => console.error(err));
+}
+
+function eliminarCategoria(id) {
+    axios.delete(`/categoria/eliminarCategoria/${id}`)
+        .then(response => {
+            console.log('Éxito:', response.data);
+            alert('Categoría Eliminada exitosamente!');
+            window.location.href = '/registrarCategoria';
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+function getCategoriaById(id) {
+    axios.get(`/categoria/obtenerCategoriaPorId/${id}`)
+        .then(function (response) {
+            const categoria = response.data;
+            const tbody = document.querySelector("tbody");
+            let htmlContent = '';
+            if (categoria) {
+                htmlContent += `
                         <tr>
                             <td>${categoria.nombre}</td>
                             <td>${categoria.descripcion}</td>
@@ -47,26 +47,26 @@
                             <td><button onclick="eliminarMarca(${categoria.id})">Eliminar</button></td>
                         </tr>
                     `;
-                }
-                tbody.innerHTML = htmlContent;
-            })
-            .catch((err) => console.error(err));
-        }
-        
+            }
+            tbody.innerHTML = htmlContent;
+        })
+        .catch((err) => console.error(err));
+}
 
-        document.addEventListener("DOMContentLoaded", function() {
-            const filtroInput = document.getElementById('filtro');
-            
-            // Cargar usuarios al inicio
+
+document.addEventListener("DOMContentLoaded", function () {
+    const filtroInput = document.getElementById('filtro');
+
+    // Cargar usuarios al inicio
+    getCategorias();
+
+    // Evento de entrada para el campo de filtro
+    filtroInput.addEventListener('input', function () {
+        const filtroValue = filtroInput.value;
+        if (filtroValue.trim() !== "") {
+            getCategoriaById(filtroValue);
+        } else {
             getCategorias();
-            
-            // Evento de entrada para el campo de filtro
-            filtroInput.addEventListener('input', function() {
-                const filtroValue = filtroInput.value;
-                if (filtroValue.trim() !== "") {
-                    getCategoriaById(filtroValue);
-                } else {
-                    getCategorias();
-                }
-            });
-        });
+        }
+    });
+});
