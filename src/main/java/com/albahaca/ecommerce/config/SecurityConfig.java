1e27532c63f8a9b94f86a0aa974a62a3591c50cb
Carnino <tinchoogd01@gmail.com>
2024-09-25 19:47:06 -0300

    estetica + logo

--- src/main/java/com/albahaca/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/albahaca/ecommerce/config/SecurityConfig.java
@@ -1,15 +1,15 @@
 package com.albahaca.ecommerce.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
@@ -22,7 +22,7 @@ public class SecurityConfig {
         return http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/css/**", "/js/**", "/images/**").permitAll()  // Permitir acceso a recursos estáticos
+                .requestMatchers("/", "/css/**", "/js/**", "/imgs/**").permitAll()  // Permitir acceso a recursos estáticos
                 .requestMatchers("/", 
                                  "/marca/listarMarcas", 
                                  "/categoria/listarCategorias", 
