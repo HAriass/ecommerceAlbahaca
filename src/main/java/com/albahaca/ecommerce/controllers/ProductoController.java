a87afdce3ace7253d19e0ba98fc1a596c8259ddb
Facundo Sanchez <105307321+facusanchez17@users.noreply.github.com>
2024-09-26 01:07:48 -0300

    filtrado productos

--- src/main/java/com/albahaca/ecommerce/controllers/ProductoController.java
+++ src/main/java/com/albahaca/ecommerce/controllers/ProductoController.java
@@ -3,7 +3,10 @@ package com.albahaca.ecommerce.controllers;
 import com.albahaca.ecommerce.models.ProductoModel;
 import com.albahaca.ecommerce.services.ProductoService;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,31 +20,61 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/producto")
 public class ProductoController {
-    
+
     @Autowired
     ProductoService productoService;
-    
+
     @GetMapping("/listarProductos")
-    public ArrayList<ProductoModel> listarProductos(){
+    public ArrayList<ProductoModel> listarProductos() {
         return this.productoService.listarProducto();
     }
-    
+
     @PostMapping("/guardarProducto")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public ProductoModel guardarProducto(@RequestBody ProductoModel producto){
+    public ProductoModel guardarProducto(@RequestBody ProductoModel producto) {
         return this.productoService.guardarProducto(producto);
     }
-    
+
     @DeleteMapping("/eliminarProducto/{id}")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public boolean eliminaProducto(@PathVariable("id") Long id){
+    public boolean eliminaProducto(@PathVariable("id") Long id) {
         return this.productoService.eliminarProducto(id);
     }
-    
+
     @GetMapping("/obtenerProductoPorId/{id}")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public Optional<ProductoModel> obtenerProductoPorId(@PathVariable("id") Long id){
+    public Optional<ProductoModel> obtenerProductoPorId(@PathVariable("id") Long id) {
         return this.productoService.obtenerProductoPorId(id);
     }
-    
+
+    @GetMapping("/obtenerProductoPorNombre/{nombreFiltro}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public List<ProductoModel> obtenerProductoPorNombre(@PathVariable("nombreFiltro") String nombreFiltro) {
+        // utilizo le metodo listarProductos para obtener todos los productos y luego
+        // filtrar por nombre
+
+        ArrayList<ProductoModel> productos = this.productoService.listarProducto();
+
+        // Filtrar por nombre sin tener en cuenta mayúsculas y minúsculas
+        return productos.stream()
+                .filter(producto -> producto.getNombre().toLowerCase().contains(nombreFiltro.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/obtenerProductoPorMarcaCategoria/{marcaCategoriaFiltro}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public List<ProductoModel> obtenerProductoPorMarcaCategoria(@PathVariable("marcaCategoriaFiltro") String marcaCategoriaFiltro) {
+
+        // Obtengo la lista de todos los productos
+        ArrayList<ProductoModel> productos = this.productoService.listarProducto();
+
+        // Filtrar productos por marca o categoría (sin tener en cuenta
+        // mayúsculas/minúsculas)
+        return productos.stream()
+                .filter(producto -> producto.getMarca().getNombre().toLowerCase()
+                        .contains(marcaCategoriaFiltro.toLowerCase()) ||
+                        producto.getCategoria().getNombre().toLowerCase().contains(marcaCategoriaFiltro.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
 }
--- src/main/resources/static/js/productos.js
+++ src/main/resources/static/js/productos.js
@@ -1,13 +1,13 @@
 function getProductos() {
-            axios.get("/producto/listarProductos")
-                .then(function(response) {
-                    const productos = response.data;
-                    const tbody = document.querySelector("tbody");
-                    let htmlContent = '';
-                    productos.forEach(producto => {
-                        // Comprobar que producto, marca y categoría existen
-                        if (producto && producto.marca && producto.categoria) {
-                            htmlContent += `
+    axios.get("/producto/listarProductos")
+        .then(function (response) {
+            const productos = response.data;
+            const tbody = document.querySelector("tbody");
+            let htmlContent = '';
+            productos.forEach(producto => {
+                // Comprobar que producto, marca y categoría existen
+                if (producto && producto.marca && producto.categoria) {
+                    htmlContent += `
                                 <tr>
                                     <td title="${producto.nombre}">${producto.nombre}</td>
                                     <td title="${producto.descripcion}">${producto.descripcion}</td>
@@ -19,34 +19,34 @@ function getProductos() {
                                     <td><button class="btn-eliminar" onclick="eliminarProducto(${producto.id})">Eliminar</button></td>
                                 </tr>
                             `;
-                        } else {
-                            console.warn('Producto o propiedades faltantes:', producto);
-                        }
-                    });
-
-                    tbody.innerHTML = htmlContent;
-                })
-                .catch((err) => console.error(err));
-        }
-        
-        function eliminarProducto(id) {
-            axios.delete(`/producto/eliminarProducto/${id}`)
-            .then(response => {
-                console.log('Éxito:', response.data);
-                eliminar();
-            })
-            .catch(error => {
-                console.error('Error:', error);
+                } else {
+                    console.warn('Producto o propiedades faltantes:', producto);
+                }
             });
-        }
-        function getProductoById(id) {
-            axios.get(`/producto/obtenerProductoPorId/${id}`)
-            .then(function(response) {
-                const producto = response.data;
-                const tbody = document.querySelector("tbody");  
-                let htmlContent = '';
-                if (producto) {
-                    htmlContent += `
+
+            tbody.innerHTML = htmlContent;
+        })
+        .catch((err) => console.error(err));
+}
+
+function eliminarProducto(id) {
+    axios.delete(`/producto/eliminarProducto/${id}`)
+        .then(response => {
+            console.log('Éxito:', response.data);
+            eliminar();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+function getProductoById(id) {
+    axios.get(`/producto/obtenerProductoPorId/${id}`)
+        .then(function (response) {
+            const producto = response.data;
+            const tbody = document.querySelector("tbody");
+            let htmlContent = '';
+            if (producto) {
+                htmlContent += `
                         <tr>
                             <td>${producto.nombre}</td>
                             <td>${producto.descripcion}</td>
@@ -63,39 +63,129 @@ function getProductos() {
                             <td><button class="btn-eliminar" onclick="eliminarProducto(${producto.id})">Eliminar</button></td>
                         </tr>
                     `;
-                }
-                tbody.innerHTML = htmlContent;
-            })
-            .catch((err) => console.error(err));
-        }
-        
-
-        document.addEventListener("DOMContentLoaded", function() {
-            const filtroInput = document.getElementById('filtro');
-            
-            // Cargar usuarios al inicio
-            getProductos();
-            
-            // Evento de entrada para el campo de filtro
-            filtroInput.addEventListener('input', function() {
-                const filtroValue = filtroInput.value;
-                if (filtroValue.trim() !== "") {
-                    getProductoById(filtroValue);
-                } else {
-                    getProductos();
-                }
-            });
-        });
-        
-        function eliminar(){ {
-              Swal.fire({
-                title: "Eliminado!",
-                text: "Registro eliminado correctamente.",
-                showConfirmButton: false,
-                icon: "success"
-              });
-              setTimeout(() => {
-                window.location.reload();
-            }, 1500);
             }
+            tbody.innerHTML = htmlContent;
+        })
+        .catch((err) => console.error(err));
+}
+
+function getProductoByName(nombreFiltro) {
+    axios.get(`/producto/obtenerProductoPorNombre/${nombreFiltro}`)
+        .then(function (response) {
+            const productos = response.data; // Recibimos un array de productos
+            const tbody = document.querySelector("tbody");
+            let htmlContent = '';
+
+            if (productos && productos.length > 0) { // Verificamos si hay productos
+                productos.forEach(producto => {
+                    htmlContent += `
+                        <tr>
+                            <td>${producto.nombre}</td>
+                            <td>${producto.descripcion}</td>
+                            <td>${producto.precio}</td>
+                            <td>
+                               ${producto.marca.nombre}
+                            </td>
+                            <td>
+                               ${producto.categoria.nombre}
+                            </td>
+                            <td><a class="btn-imagen" href="${producto.imagen}" target="_blank">Ver imagen</a></td>
+                            <td><button class="btn-modificar" onclick="location.href='/modificarProducto/${producto.id}'">Modificar</button></td>
+                            <td><button class="btn-eliminar" onclick="eliminarProducto(${producto.id})">Eliminar</button></td>
+                        </tr>
+                    `;
+                });
+            } else {
+                htmlContent = '<tr><td colspan="8">No se encontraron productos.</td></tr>';
+            }
+
+            tbody.innerHTML = htmlContent;
+        }).catch((err) => console.error(err));
+}
+
+function getProductoByMarcaCategoria(marcaCategoriaFiltro) {
+    axios.get(`/producto/obtenerProductoPorMarcaCategoria/${marcaCategoriaFiltro}`)
+        .then(function (response) {
+            const productos = response.data; // Recibimos un array de productos
+            const tbody = document.querySelector("tbody");
+            let htmlContent = '';
+
+            if (productos && productos.length > 0) { // Verificamos si hay productos
+                productos.forEach(producto => {
+                    htmlContent += `
+                        <tr>
+                            <td>${producto.nombre}</td>
+                            <td>${producto.descripcion}</td>
+                            <td>${producto.precio}</td>
+                            <td>
+                               ${producto.marca.nombre}
+                            </td>
+                            <td>
+                               ${producto.categoria.nombre}
+                            </td>
+                            <td><a class="btn-imagen" href="${producto.imagen}" target="_blank">Ver imagen</a></td>
+                            <td><button class="btn-modificar" onclick="location.href='/modificarProducto/${producto.id}'">Modificar</button></td>
+                            <td><button class="btn-eliminar" onclick="eliminarProducto(${producto.id})">Eliminar</button></td>
+                        </tr>
+                    `;
+                });
+            } else {
+                htmlContent = '<tr><td colspan="8">No se encontraron productos.</td></tr>';
+            }
+
+            tbody.innerHTML = htmlContent;
+        }).catch((err) => console.error(err));
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const filtroInput = document.getElementById('filtro');
+    const filtroName = document.getElementById('filtroName');
+    const filtroMarca = document.getElementById('filtroMarca');
+
+    // Cargar usuarios al inicio
+    getProductos();
+
+    // Evento de entrada para el campo de filtro
+    filtroInput.addEventListener('input', function () {
+        const filtroValue = filtroInput.value;
+        if (filtroValue.trim() !== "") {
+            getProductoById(filtroValue);
+        } else {
+            getProductos();
         }
+    });
+
+    filtroName.addEventListener('input', function () {
+        const filtroValue = filtroName.value;
+        if (filtroValue.trim() !== "") {
+            getProductoByName(filtroValue);
+        } else {
+            getProductos();
+        }
+    });
+
+    filtroMarca.addEventListener('input', function () {
+        const filtroValue = filtroMarca.value;
+        if (filtroValue.trim() !== "") {
+            getProductoByMarcaCategoria(filtroValue);
+        } else {
+            getProductos();
+        }
+    });
+
+});
+
+function eliminar() {
+    {
+        Swal.fire({
+            title: "Eliminado!",
+            text: "Registro eliminado correctamente.",
+            showConfirmButton: false,
+            icon: "success"
+        });
+        setTimeout(() => {
+            window.location.reload();
+        }, 1500);
+    }
+}
