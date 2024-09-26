170d3d73963bbc16ffd447c33948fc1e3ed255b6
Facundo Sanchez <105307321+facusanchez17@users.noreply.github.com>
2024-09-26 10:18:58 -0300

    filtrado estados

--- src/main/java/com/albahaca/ecommerce/controllers/EstadoController.java
+++ src/main/java/com/albahaca/ecommerce/controllers/EstadoController.java
@@ -1,10 +1,14 @@
 
 package com.albahaca.ecommerce.controllers;
 
+import com.albahaca.ecommerce.models.CategoriaModel;
 import com.albahaca.ecommerce.models.EstadoModel;
 import com.albahaca.ecommerce.services.EstadoService;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,28 +23,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/estado")
 public class EstadoController {
     @Autowired
-    EstadoService estadoService ;
-    
+    EstadoService estadoService;
+
     @GetMapping("/listarEstados")
-    public ArrayList<EstadoModel> obtenerEstado(){
+    public ArrayList<EstadoModel> obtenerEstado() {
         return estadoService.obtenerEstados();
     }
-    
+
     @PostMapping("/guardarEstado")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public EstadoModel guardarEstado(@RequestBody EstadoModel estado){
-        return this.estadoService.guardarEstado(estado);   
+    public EstadoModel guardarEstado(@RequestBody EstadoModel estado) {
+        return this.estadoService.guardarEstado(estado);
     }
-    
+
     @DeleteMapping("/eliminarEstado/{id}")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public boolean eliminarEstado(@PathVariable("id") Long id){
-        return this.estadoService.eliminarEstado(id);   
+    public boolean eliminarEstado(@PathVariable("id") Long id) {
+        return this.estadoService.eliminarEstado(id);
     }
-    
+
     @GetMapping("/obtenerEstadoPorId/{id}")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public Optional<EstadoModel> obtenerEstadoPorId(@PathVariable("id") Long id){
+    public Optional<EstadoModel> obtenerEstadoPorId(@PathVariable("id") Long id) {
         return this.estadoService.obtenerEstadoPorId(id);
+    }
+
+    @GetMapping("/obtenerEstadoPorNombre/{filtroName}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public List<EstadoModel> obtenerEstadoPorId(@PathVariable("filtroName") String filtroName) {
+        ArrayList<EstadoModel> estados = this.estadoService.obtenerEstados();
+
+        // Filtrar por nombre sin tener en cuenta mayúsculas y minúsculas
+        return estados.stream()
+                .filter(estado -> estado.getNombre().toLowerCase().contains(filtroName.toLowerCase()))
+                .collect(Collectors.toList());
     }
 }
--- src/main/resources/static/js/estados.js
+++ src/main/resources/static/js/estados.js
@@ -1,9 +1,65 @@
-        function getEstados() {
-            axios.get("/estado/listarEstados")
-            .then(function(response) {
-                const estados = response.data;
-                const tbody = document.querySelector("tbody");
-                let htmlContent = '';
+function getEstados() {
+    axios.get("/estado/listarEstados")
+        .then(function (response) {
+            const estados = response.data;
+            const tbody = document.querySelector("tbody");
+            let htmlContent = '';
+            estados.forEach(estado => {
+                htmlContent += `
+                        <tr>
+                            <td>${estado.nombre}</td>
+                            <td>${estado.descripcion}</td>
+                            <td><button class="btn-modificar" onclick="location.href='/modificarEstado/${estado.id}'">Modificar</button></td>
+                            <td><button class="btn-eliminar" onclick="eliminarEstado(${estado.id})">Eliminar</button></td>
+                        </tr>
+                    `;
+            });
+            tbody.innerHTML = htmlContent;
+        })
+        .catch((err) => console.error(err));
+}
+
+function eliminarEstado(id) {
+    axios.delete(`/estado/eliminarEstado/${id}`)
+        .then(response => {
+            console.log('Éxito:', response.data);
+            eliminar();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+function getEstadoById(id) {
+    axios.get(`/estado/obtenerEstadoPorId/${id}`)
+        .then(function (response) {
+            const estado = response.data;
+            const tbody = document.querySelector("tbody");
+            let htmlContent = '';
+            if (estado) {
+                htmlContent += `
+                        <tr>
+                            <td>${estado.nombre}</td>
+                            <td>${estado.descripcion}</td>
+                            <td><button class="btn-modificar" onclick="location.href='/modificarEstado/${estado.id}'">Modificar</button></td>
+                            <td><button class="btn-eliminar" onclick="eliminarEstado(${estado.id})">Eliminar</button></td>
+                        </tr>
+                    `;
+            } else {
+                htmlContent = '<tr><td colspan="4">No se encontraron estados.</td></tr>';
+            }
+            tbody.innerHTML = htmlContent;
+        })
+        .catch((err) => console.error(err));
+}
+
+function getEstadoByName(filtroName) {
+    axios.get(`/estado/obtenerEstadoPorNombre/${filtroName}`)
+        .then(function (response) {
+            const estados = response.data;
+            const tbody = document.querySelector("tbody");
+            let htmlContent = '';
+            if (estados && estados.length > 0) {
                 estados.forEach(estado => {
                     htmlContent += `
                         <tr>
@@ -14,70 +70,53 @@
                         </tr>
                     `;
                 });
-                tbody.innerHTML = htmlContent;
-            })
-            .catch((err) => console.error(err));
-        }
-        
-        function eliminarEstado(id) {
-            axios.delete(`/estado/eliminarEstado/${id}`)
-            .then(response => {
-                console.log('Éxito:', response.data);
-                eliminar();
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-        }
-        
-        function getEstadoById(id) {
-            axios.get(`/estado/obtenerEstadoPorId/${id}`)
-            .then(function(response) {
-                const estado = response.data;
-                const tbody = document.querySelector("tbody");
-                let htmlContent = '';
-                if (estado) {
-                    htmlContent += `
-                        <tr>
-                            <td>${estado.nombre}</td>
-                            <td>${estado.descripcion}</td>
-                            <td><button class="btn-modificar" onclick="location.href='/modificarEstado/${estado.id}'">Modificar</button></td>
-                            <td><button class="btn-eliminar" onclick="eliminarEstado(${estado.id})">Eliminar</button></td>
-                        </tr>
-                    `;
-                }
-                tbody.innerHTML = htmlContent;
-            })
-            .catch((err) => console.error(err));
-        }
-        
-        document.addEventListener("DOMContentLoaded", function() {
-            const filtroInput = document.getElementById('filtro');
-            
-            // Cargar estados al inicio
-            getEstados();
-            
-            // Evento de entrada para el campo de filtro
-            filtroInput.addEventListener('input', function() {
-                const filtroValue = filtroInput.value;
-                if (filtroValue.trim() !== "") {
-                    getEstadoById(filtroValue);
-                } else {
-                    getEstados();
-                }
-            });
-        });
-        
-        function eliminar(){ {
-              Swal.fire({
-                title: "Eliminado!",
-                text: "Registro eliminado correctamente.",
-                showConfirmButton: false,
-                icon: "success"
-              });
-              setTimeout(() => {
-                window.location.reload();
-            }, 1500);
+            } else {
+                htmlContent = '<tr><td colspan="4">No se encontraron estados.</td></tr>';
             }
+            tbody.innerHTML = htmlContent;
+        })
+        .catch((err) => console.error(err));
+
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const filtroInput = document.getElementById('filtro');
+    const filtroName = document.getElementById('filtroName');
+
+    // Cargar estados al inicio
+    getEstados();
+
+    // Evento de entrada para el campo de filtro
+    filtroInput.addEventListener('input', function () {
+        const filtroValue = filtroInput.value;
+        if (filtroValue.trim() !== "") {
+            getEstadoById(filtroValue);
+        } else {
+            getEstados();
         }
-        
+    });
+
+    filtroName.addEventListener('input', function () {
+        const filtroValue = filtroName.value;
+        if (filtroValue.trim() !== "") {
+            getEstadoByName(filtroValue);
+        } else {
+            getEstados();
+        }
+    });
+
+});
+
+function eliminar() {
+    {
+        Swal.fire({
+            title: "Eliminado!",
+            text: "Registro eliminado correctamente.",
+            showConfirmButton: false,
+            icon: "success"
+        });
+        setTimeout(() => {
+            window.location.reload();
+        }, 1500);
+    }
+}
