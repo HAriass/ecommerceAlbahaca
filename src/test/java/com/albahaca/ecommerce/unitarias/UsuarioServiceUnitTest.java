75528e595da6bca97907243f901dd5f4b8646d35
martin-coser <cosermartin@hotmail.com>
2024-10-17 10:40:51 -0300

    test carpetas

---
+++ src/test/java/com/albahaca/ecommerce/unitarias/UsuarioServiceUnitTest.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.albahaca.ecommerce.unitarias;
+
+/**
+ *
+ * @author coser
+ */
+public class UsuarioServiceUnitTest {
+    
+}
